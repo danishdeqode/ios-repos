6623af76f0803d67bd4f90aa3c5d319ebbf41ba1
Danish Khan <danishdev64@gmail.com>
2023-01-15 12:48:33 +0530

    Add trader app

---
+++ navigation/navigation/firstScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigation
+//
+//  Created by danish on 14/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
