90c39241ac5a18938e75d9fa8bb5df14e0cbf560
danish khan <danishlnct@gmail.com>
2023-03-06 13:19:08 +0530

    update playground code day 2

--- playground-codes/day 2.playground/Contents.swift
+++ playground-codes/day 2.playground/Contents.swift
@@ -13,18 +13,85 @@ import UIKit
 
 //=====================================================
 
-struct Point: Equatable {
-    var x: Int
-}
+//struct Point: Equatable {
+//    var x: Int
+//}
+//
+//struct X {
+//    var point: Point {
+//        didSet {
+//            point.x = 0
+//        }
+//    }
+//}
+//
+//let y = Point(x: 5)
+//var p = X(point: y)
+//let result = p.point.x == y.x
 
-struct X {
-    var point: Point {
-        didSet {
-            point.x = 0
-        }
+
+
+//=====================================================
+
+
+//
+//@objc class Person: NSObject {
+//    @objc dynamic var name = "Taylor Swift"
+//    @objc dynamic var email = "aaa"
+//}
+//
+//let taylor = Person()
+//
+//
+//taylor.observe(\Person.name, options: .new) { person, change in
+//    print("I'm now called \(person.name)")
+//}
+//
+//taylor.observe(\Person.email, options: .new) { person, change in
+//    print("I'm now called email \(person.email)")
+//}
+//
+//taylor.name = "Justin Bieber"
+//taylor.email = "yyyy"
+
+
+
+//=====================================================
+
+
+//For serial execution
+let execute =  DispatchQueue(label: "test")
+execute.sync {
+    print("task 1 start =>")
+    for i in 0...10{
+        print("task 1: \(i)")
     }
+
+    print("task 1 end =>")
+}
+execute.sync {
+    print("task 2 start =>")
+    for i in 0...10{
+        print("task 2: \(i)")
+    }
+    print("task 2 end =>")
 }
 
-let y = Point(x: 5)
-var p = X(point: y)
-let result = p.point.x == y.x
+
+//For concurrent execution
+let execute1 =  DispatchQueue(label: "test", attributes: .concurrent)
+execute1.async {
+    print("task 1 start =>")
+    for i in 0...10{
+        print("task 1: \(i)")
+    }
+    
+    print("task 1 end =>")
+}
+execute1.async {
+    print("task 2 start =>")
+    for i in 0...10{
+        print("task 2: \(i)")
+    }
+    print("task 2 end =>")
+}
